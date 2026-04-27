d2e113a2565a1cf44c51f4d0b356a0f6dd1b487b
sagarsp19-cpu <sagarpatil079@gmail.com>
2026-04-27 13:05:37 +0530

    Update App.java

--- Amazon/Amazon-Core/src/main/java/org/amazon/App.java
+++ Amazon/Amazon-Core/src/main/java/org/amazon/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! new" );
-        System.out.println( "Welcome to ABC " );
-        System.out.println( "Welcome to ABC " );
+        System.out.println( "welcome" );
+       
     }
 }
